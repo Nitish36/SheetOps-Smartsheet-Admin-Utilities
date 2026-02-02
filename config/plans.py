66e2743bd879565cccc53c84193e0d1065c11021
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2026-02-02 19:13:09 +0530

    Final Commit

---
+++ config/plans.py
@@ -0,0 +1,17 @@
+PLANS = {
+    "trial": {
+        "max_rows": 50,
+        "deep_scrape": False,
+        "script_path": "trial"
+    },
+    "paid": {
+        "max_rows": None,
+        "deep_scrape": False,
+        "script_path": "full"
+    },
+    "enterprise": {
+        "max_rows": None,
+        "deep_scrape": True,
+        "script_path": "enterprise"
+    }
+}
